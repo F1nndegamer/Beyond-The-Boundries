eebe869d537a5d4bf96964bbd80e548c7ecc946c
icantdo <80340838+icantdo@users.noreply.github.com>
2025-09-13 13:32:21 +0300

    knockback and simpleglitch update

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float knockbackForce = 10f;  // Push
+    public float knockbackDuration = 0.2f; // The player loses control during this period.
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+
+            if (playerRb != null)
+            {
+                
+                Vector2 knockbackDir = (collision.transform.position - transform.position).normalized;
+
+                playerRb.AddForce(knockbackDir * knockbackForce, ForceMode2D.Impulse);
+
+                PlayerMovement movement = collision.gameObject.GetComponent<PlayerMovement>();
+                if (movement != null)
+                {
+                    movement.enabled = false;
+                    collision.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(ReEnableMovement(movement));
+                }
+            }
+        }
+    }
+
+    private System.Collections.IEnumerator ReEnableMovement(PlayerMovement movement)
+    {
+        yield return new WaitForSeconds(knockbackDuration);
+        movement.enabled = true;
+    }
+}
--- Assets/Scripts/Glitches/SimpleGlitch.cs
+++ Assets/Scripts/Glitches/SimpleGlitch.cs
@@ -1,42 +1,114 @@
-using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SimpleGlitch : MonoBehaviour
 {
-    SpriteRenderer sr;
-    Vector3 originalPos;
-    Color originalColor;
+    public Tilemap tilemap;                         
+    /*public bool useBounds = false;                  
+    public Vector3Int boundsMin = Vector3Int.zero;  
+    public Vector3Int boundsMax = Vector3Int.one; */ 
 
-    public float glitchChance = 0.05f; 
-    public float glitchOffset = 0.1f;  
+    public List<Vector3Int> targetCells = new List<Vector3Int>();
+
+    public float glitchChance = 0.05f;
+    public float glitchOffset = 0.1f;   
     public float glitchTime = 0.05f;
     public bool startGlitch = false;
 
+    TilemapCollider2D tilemapCollider;
+
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
-        originalPos = transform.localPosition;
-        originalColor = sr.color;
+        tilemapCollider = tilemap.GetComponent<TilemapCollider2D>();
     }
 
     void Update()
     {
-        if (startGlitch & Random.value < glitchChance)
+        
+        if (startGlitch && Random.value < glitchChance)
+        {
             StartCoroutine(DoGlitch());
-        //else if (!startGlitch)
-            //GetComponent<Collider2D>().enabled = true;
+        }
     }
 
-    System.Collections.IEnumerator DoGlitch()
+    IEnumerator DoGlitch()
     {
-        //GetComponent<Collider2D>().enabled = false;
-        transform.localPosition = originalPos + new Vector3(Random.Range(-glitchOffset, glitchOffset), 0, 0);
+        
+        Vector3Int cell = PickRandomCell();
+        if (cell == null) yield break;
 
-        sr.color = new Color(Random.value, Random.value, Random.value, sr.color.a);
+        TileBase tb = tilemap.GetTile(cell);
+        if (tb == null)
+            yield break; 
+
+        Matrix4x4 originalMatrix = tilemap.GetTransformMatrix(cell);
+        Color originalColor = tilemap.GetColor(cell);
+
+       
+        if (tilemapCollider != null)
+            tilemapCollider.enabled = false;
+
+
+        float offsetX = Random.Range(-glitchOffset, glitchOffset);
+        Vector3 translation = new Vector3(offsetX, 0f, 0f);
+
+        Matrix4x4 glitchMatrix = Matrix4x4.TRS(translation, Quaternion.identity, Vector3.one);
+        tilemap.SetTransformMatrix(cell, glitchMatrix);
+
+        Color glitchColor = new Color(Random.value, Random.value, Random.value, originalColor.a);
+        tilemap.SetColor(cell, glitchColor);
 
         yield return new WaitForSeconds(glitchTime);
 
-        transform.localPosition = originalPos;
-        sr.color = new Color(originalColor.r, originalColor.g,originalColor.b, sr.color.a);
+        tilemap.SetTransformMatrix(cell, originalMatrix);
+        tilemap.SetColor(cell, originalColor);
+
+        if (tilemapCollider != null)
+            tilemapCollider.enabled = true;
+    }
+
+    Vector3Int PickRandomCell()
+    {
+
+        /*if (useBounds)
+        {
+            List<Vector3Int> cellsInBounds = new List<Vector3Int>();
+            for (int x = boundsMin.x; x <= boundsMax.x; x++)
+                for (int y = boundsMin.y; y <= boundsMax.y; y++)
+                    for (int z = boundsMin.z; z <= boundsMax.z; z++)
+                    {
+                        Vector3Int c = new Vector3Int(x, y, z);
+                        if (tilemap.HasTile(c)) cellsInBounds.Add(c);
+                    }
+
+            if (cellsInBounds.Count == 0) return default;
+            return cellsInBounds[Random.Range(0, cellsInBounds.Count)];
+        }*/ //didn't work
+        if (true)
+        {
+            // Select randomly from the targetCells list (if there is nothing in the list, select a random tile from the tilemap)
+            if (targetCells != null && targetCells.Count > 0)
+            {
+                return targetCells[Random.Range(0, targetCells.Count)];
+            }
+            else
+            {
+                // fallback: randomly select a filled cell from the tilemap's cellBounds
+                var bounds = tilemap.cellBounds;
+                List<Vector3Int> filled = new List<Vector3Int>();
+                for (int x = bounds.xMin; x <= bounds.xMax; x++)
+                {
+                    for (int y = bounds.yMin; y <= bounds.yMax; y++)
+                    {
+                        Vector3Int c = new Vector3Int(x, y, 0);
+                        if (tilemap.HasTile(c)) filled.Add(c);
+                    }
+                }
+                if (filled.Count == 0) return default;
+                return filled[Random.Range(0, filled.Count)];
+            }
+        }
     }
 }
