0708ac3c9c4279a99c0a868ed174e5acc4e209c3
F1nndegamer <140960106+F1nndegamer@users.noreply.github.com>
2025-09-14 12:54:13 +0200

    New Settings+Font addition

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
